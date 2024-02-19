5bc68b8e2fbef8d023413912031746da25e518a1
Matt Trentini <matt.trentini@gmail.com>
2024-02-19 22:46:51 +1100

    Update README and tidy up implementation

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # micropython-hexdump
-An implementation of Hexdump for MicroPython.
 
-Initial implementation kindly donated by [Kongduino](https://github.com/Kongduino/).
+A simple implementation of hexdump (`hd` and `xxd`) tools for MicroPython.
 
 ## Installation
 
@@ -11,9 +10,35 @@ Use `mip` from the command line using `mpremote`:
 > mpremote mip install github:mattytrentini/micropython-hexdump
 ```
 
-or from within a running instance of MicroPython that has an established internet connection.
+or from within a running instance of MicroPython that has an established
+internet connection.
 
 ```python
 >>> import mip
 >>> mip.install("github:mattytrentini/micropython-hexdump")
+```
+
+## Example usage
+
+```text
+>>> from os import urandom
+>>> from hexdump import hd, xxd
+>>> data = urandom(100)
+>>> hd(data)
+00000000  4d 9a 16 3d 4c 87 c4 31  9e 95 36 e3 f8 49 4b 4b  |M..=L..1..6..IKK|
+00000010  98 12 6b b6 a6 a3 fd 1b  91 a5 21 95 73 ac 35 6f  |..k.......!.s.5o|
+00000020  dc b4 4d 0b 43 fb bb 36  d6 17 52 4d 40 b4 04 ed  |..M.C..6..RM@...|
+00000030  2b 7c 8b 30 84 a3 96 9a  71 e4 e5 69 d1 62 b7 06  |+|.0....q..i.b..|
+00000040  fd 89 3a f7 b3 06 04 39  f9 70 62 33 d2 56 35 e2  |..:....9.pb3.V5.|
+00000050  fc 7e 16 46 5f 35 1d 62  63 d4 5c 18 f3 de 6d 3c  |.~.F_5.bc.\...m<|
+00000060  a1 1f fa ed                                       |....|
+00000061
+>>> xxd(data)
+00000000: 4d 9a 16 3d 4c 87 c4 31 9e 95 36 e3 f8 49 4b 4b  M..=L..1..6..IKK
+00000010: 98 12 6b b6 a6 a3 fd 1b 91 a5 21 95 73 ac 35 6f  ..k.......!.s.5o
+00000020: dc b4 4d 0b 43 fb bb 36 d6 17 52 4d 40 b4 04 ed  ..M.C..6..RM@...
+00000030: 2b 7c 8b 30 84 a3 96 9a 71 e4 e5 69 d1 62 b7 06  +|.0....q..i.b..
+00000040: fd 89 3a f7 b3 06 04 39 f9 70 62 33 d2 56 35 e2  ..:....9.pb3.V5.
+00000050: fc 7e 16 46 5f 35 1d 62 63 d4 5c 18 f3 de 6d 3c  .~.F_5.bc.\...m<
+00000060: a1 1f fa ed                                      ....
 ```
--- hexdump.py
+++ hexdump.py
@@ -1,36 +1,21 @@
-def hexdump(buf):
-    buf = bytearray(buf)
-    alphabet = b"0123456789abcdef"
-    ln = len(buf)
-    # print("Len of buf: "+str(ln))
-    print("   +------------------------------------------------+ +----------------+")
-    print("   |.0 .1 .2 .3 .4 .5 .6 .7 .8 .9 .a .b .c .d .e .f | |      ASCII     |")
-    i = 0
-    while i < ln:
-        if i % 128 == 0:
-            print(
-                "   +------------------------------------------------+ +----------------+"
-            )
-        s = b"|                                                | |................|"
-        s0 = list(s)
-        ix = 1
-        iy = 52
-        j = 0
-        while j < 16:
-            if (i + j) < ln:
-                c = buf[i + j]
-                s0[ix] = alphabet[(c >> 4) & 0x0F]
-                ix += 1
-                s0[ix] = alphabet[c & 0x0F]
-                ix += 2
-                if c > 31 and c < 127:
-                    s0[iy] = c
-                iy += 1
-            j += 1
-        index = int(i / 16)
-        hd = ("00" + hex(index)[2:])[-2:] + "."
-        s = bytearray(s0)
-        s = hd + s.decode()
-        print(s)
-        i += 16
-    print("   +------------------------------------------------+ +----------------+")
+def hd(buf):
+    m = memoryview(buf)
+    for i in range(0, len(m), 16):
+        line = m[i:i+16]
+        print("{:08x}  ".format(i),
+            line[:8].hex(" "), "  ",
+            line[8:].hex(" ") if len(line) > 8 else "  ",
+            " " * 3 * (16-len(line)-1),
+            "  |",
+            "".join(chr(x) if 31 < x < 127 else "." for x in line),
+            "|", sep="")
+    print("{:08x}  ".format(i+1))
+
+def xxd(buf):
+    m = memoryview(buf)
+    for i in range(0, len(m), 16):
+        line = m[i:i+16]
+        print("{:08x}: {}{}  {}".format(i,
+            line.hex(" "),
+            " " * 3 * (16-len(line)),
+            "".join(chr(x) if 31 < x < 127 else "." for x in line)))
